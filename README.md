3de1e4342037a7a3ac3e92d6bce0404e59e5221f
Arjun Venkataswamy <avenkat2@gmail.com>
2015-07-08 11:46:01 -0500

    adds challenge solutions and edits readme

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Test you understanding by extending the application in the following ways:
 
 1. Display the latitude and longitude of the address that's entered
 2. Display the wind speed and direction
-3. Display the maximum and minimum temperature for the day (This website might be helpful - http://www.metric-conversions.org/temperature/kelvin-to-fahrenheit.htm)
+3. Display the current temperature (This website might be helpful - http://www.metric-conversions.org/temperature/kelvin-to-fahrenheit.htm)
 
 **Challenge.** Display the sunrise and sunset at the address that's entered. You'll need to figure out how to convert the given data into a readable time
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,11 +6,23 @@ class ApiController < ApplicationController
   end
 
   def weather_output
+    # preprocesses form info
     address = URI.encode(params["address"])
+
+    # dynamically generates URL
     url = "http://api.openweathermap.org/data/2.5/weather?q=#{address}"
+
+    # saves information at the generated URL
     initial_info = open(url).read
+
+    # converts the result into Ruby hashes and arrays
     parsed_info = JSON.parse(initial_info)
+
     @result = parsed_info["weather"][0]["main"]
     @icon = parsed_info["weather"][0]["icon"]
+    @wind_speed = parsed_info['wind']['speed']
+    @wind_dir = parsed_info['wind']['deg']
+    @current_temp_kelvin = parsed_info['main']['temp']
+    @current_temp_farenheit = (@current_temp_kelvin - 273.15)* 1.8000 + 32.00
   end
 end
